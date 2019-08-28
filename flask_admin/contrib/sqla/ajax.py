1094e7df6ca21cf7fb7d34a0ec5e5ef7abc32d9d
Florent Messa <florent.messa@gmail.com>
2019-08-28 13:09:51 +0200

    ref: remove un-necessary string cast on filters

--- flask_admin/contrib/sqla/ajax.py
+++ flask_admin/contrib/sqla/ajax.py
@@ -73,8 +73,7 @@ class QueryAjaxModelLoader(AjaxModelLoader):
         query = query.filter(or_(*filters))
 
         if self.filters:
-            filters = ["%s.%s" % (self.model.__tablename__.lower(), value) for value in self.filters]
-            query = query.filter(and_(*filters))
+            query = query.filter(and_(*self.filters))
 
         if self.order_by:
             query = query.order_by(self.order_by)
